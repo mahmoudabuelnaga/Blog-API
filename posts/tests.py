f7365aa6aaee22bbb6a8aa519e43dae534522da1
mahmoudabuelnaga <mahmoudaboelnaga392@gmail.com>
2022-05-14 01:21:19 +0200

    Add Blog Test

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Posts
 
 # Create your tests here.
+
+class BlogTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        testuser1 = User.objects.create(username="mahmoud", password="m1751998")
+        testpost1 = Posts.objects.create(author=testuser1, title="Blog Title", body="Blog Content...")
+        testuser1.save()
+        testpost1.save()
+    
+    def test_blog_content(self):
+        post = Posts.objects.get(id=1)
+        author = f"{post.author}"
+        title = f"{post.title}"
+        body = f"{post.body}"
+
+        self.assertEqual(author, "mahmoud")
+        self.assertEqual(title, "Blog Title")
+        self.assertEqual(body, "Blog Content...")
