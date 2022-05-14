db679176a9cdc735695363d3114f914cafd1592e
mahmoudabuelnaga <mahmoudaboelnaga392@gmail.com>
2022-05-14 19:47:17 +0200

    update project

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from .serializers import PostSerializer
 from .models import Posts
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 # Create your views here.
 
 class PostList(generics.ListCreateAPIView):
+    # permission_classes = (permissions.IsAuthenticated,)
     queryset = Posts.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = (permissions.IsAuthenticated,)
     queryset = Posts.objects.all()
     serializer_class = PostSerializer
